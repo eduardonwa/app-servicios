d38f6e8c1e926d8e190ee3390e31b716c91289a7
jgallegosramos <jgallegosramos@gmail.com>
2020-07-02 10:33:02 -0700

    Funcionalidad de Fragments

--- app/src/main/java/com/example/david/categories_screen.java
+++ app/src/main/java/com/example/david/categories_screen.java
@@ -1,21 +1,52 @@
 package com.example.david;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class categories_screen extends AppCompatActivity {
+
+    BottomNavigationView mBottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_screen);
+
+        mBottomNavigation = (BottomNavigationView)findViewById(R.id.bottom_navbar);
+        mBottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.settings){
+                    showSelectecFragment(new Contratar());
+                }
+
+                if(item.getItemId() == R.id.categories){
+                    showSelectecFragment(new Listado());
+                }
+
+                if(item.getItemId() == R.id.profile){
+                    showSelectecFragment(new PerfilServidor());
+                }
+
+                return true;
+            }
+        });
     }
 
-    public void Anterior(View view){
-        Intent Anterior = new Intent(this, welcome_screen.class);
-        startActivity(Anterior);
+    private void showSelectecFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
     }
+
 }
